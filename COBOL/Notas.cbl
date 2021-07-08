1fea5cf1b5c559ee0ac787173b334212b4bb9ea6
ramonidias <ramonidemorais@gmail.com>
2021-07-08 15:39:25 -0300

    V1

--- COBOL/Notas.cbl
+++ COBOL/Notas.cbl
@@ -39,13 +39,13 @@
            INITIALIZE VARIAVEIS-WORKING
            
            DISPLAY "Informe o Aluno.:" 
-           DISPLAY "Nota 1o Bimestre:" AT 0907
-           DISPLAY "Nota 2o Bimestre:" AT 1007
-           DISPLAY "Nota 3o Bimestre:" AT 1107
-           DISPLAY "Nota 4o Bimestre:" AT 1207
-           DISPLAY "Total das Notas.:" AT 1607
-           DISPLAY "Media das Notas.:" AT 1707
-           DISPLAY "Menssagem.......:" AT 2007
+           DISPLAY "Nota 1o Bimestre:"
+           DISPLAY "Nota 2o Bimestre:" 
+           DISPLAY "Nota 3o Bimestre:" 
+           DISPLAY "Nota 4o Bimestre:" 
+           DISPLAY "Total das Notas.:" 
+           DISPLAY "Media das Notas.:" 
+           DISPLAY "Menssagem.......:" 
            
            .
        10000-FIM-INICIA.
@@ -54,73 +54,73 @@
        20000-PROCESSA SECTION.
            DISPLAY "Informe o nome do aluno"
                                        
-           ACCEPT W-ALUNO              AT 0625
-           DISPLAY W-BRANCOS           AT 2025
+           ACCEPT W-ALUNO              
+           DISPLAY W-BRANCOS          
            IF KEY-ESC
-              DISPLAY "Fim"            AT 2025
+              DISPLAY "Fim"            
               STOP " "
               GO 89999-FIM-PROCESSA
            END-IF
            IF W-ALUNO = SPACES
               DISPLAY "Campo obrigatorio"
-                                       AT 2025
+                                       
               STOP " "
-              DISPLAY W-BRANCOS        AT 2025
+              DISPLAY W-BRANCOS        
               GO 20000-PROCESSA
            END-IF
            .
        20100-ENTRA-NOTA-1.
            DISPLAY "Informe a primeira nota do aluno"
-                                       AT 2025
-           ACCEPT W-NOTA1              AT 0925
-           DISPLAY W-BRANCOS           AT 2025
+                                       
+           ACCEPT W-NOTA1              
+           DISPLAY W-BRANCOS           
            IF KEY-ESC
               GO 20000-PROCESSA
            END-IF
            
            ADD W-NOTA1                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       AT 1625
+           DISPLAY W-TOTAL-NOTAS       
            .
        20200-ENTRA-NOTA-2.
            DISPLAY "Informe a segunda nota do aluno"
-                                       AT 2025
-           ACCEPT W-NOTA2              AT 1025
-           DISPLAY W-BRANCOS           AT 2025
+                                       
+           ACCEPT W-NOTA2              
+           DISPLAY W-BRANCOS           
            
            ADD W-NOTA2                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       AT 1625
+           DISPLAY W-TOTAL-NOTAS       
            .
        20300-ENTRA-NOTA-3.
            DISPLAY "Informe a terceira nota do aluno"
-                                       AT 2025
-           ACCEPT W-NOTA3              AT 1125
-           DISPLAY W-BRANCOS           AT 2025
+                                       
+           ACCEPT W-NOTA3              
+           DISPLAY W-BRANCOS           
            
            ADD W-NOTA3                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       AT 1625
+           DISPLAY W-TOTAL-NOTAS       
            .
        20400-ENTRA-NOTA-4.
            DISPLAY "Informe a primeira nota do aluno"
-                                       AT 2025
-           ACCEPT W-NOTA4              AT 1225
-           DISPLAY W-BRANCOS           AT 2025
+                                       
+           ACCEPT W-NOTA4              
+           DISPLAY W-BRANCOS           
            
            ADD W-NOTA4                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       AT 1625
+           DISPLAY W-TOTAL-NOTAS       
            .
        20500-CALCULA-MEDIA.
            DIVIDE W-TOTAL-NOTAS        BY 4
                                    GIVING W-MEDIA
-           DISPLAY W-MEDIA             AT 1725
+           DISPLAY W-MEDIA             
            
            IF W-MEDIA < 5,0
-              DISPLAY "REPROVADO!!!"   AT 2025
+              DISPLAY "REPROVADO!!!"   
            ELSE
               IF W-MEDIA < 7,0
                  DISPLAY "RECUPERACAO!!!"
-                                       AT 2025
+                                       
               ELSE
-                 DISPLAY "APROVADO!!!" AT 2025
+                 DISPLAY "APROVADO!!!"
               END-IF
            END-IF
            STOP " "
