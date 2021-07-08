821d58584e2d3e5034c3936d3a792b5d5a655ced
ramonidias <ramonidemorais@gmail.com>
2021-07-08 16:12:18 -0300

    v.1

--- COBOL/Notas.cbl
+++ COBOL/Notas.cbl
@@ -1,6 +1,6 @@
       *----------------------------------------------------------------*
        IDENTIFICATION DIVISION.
-       PROGRAM-ID.    notas.
+       PROGRAM-ID.    Notas.
       *----------------------------------------------------------------*
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
@@ -24,17 +24,20 @@
            05  W-MEDIA                 PIC 9(02) VALUE ZEROS .
       *----------------------------------------------------------------*
        PROCEDURE DIVISION.
+
        00000-PRINCIPAL SECTION.
            PERFORM 10000-INICIA
            PERFORM 20000-PROCESSA
            PERFORM 90000-FINALIZA
           
            STOP RUN
-           .
+           
        99999-FIM-PRINCIPAL.
            EXIT.
+
       *----------------------------------------------------------------*
        10000-INICIA SECTION.
+      *----------------------------------------------------------------* 
            
            INITIALIZE VARIAVEIS-WORKING
            
@@ -47,41 +50,47 @@
            DISPLAY "Media das Notas.:" 
            DISPLAY "Menssagem.......:" 
            
-           .
-       10000-FIM-INICIA.
-           EXIT.
+      
+       10000-FIM-INICIA EXIT.
+      
       *----------------------------------------------------------------*
        20000-PROCESSA SECTION.
+      *----------------------------------------------------------------* 
            DISPLAY "Informe o nome do aluno"
                                        
            ACCEPT W-ALUNO              
-           DISPLAY W-BRANCOS          
+           DISPLAY W-BRANCOS  
+
            IF KEY-ESC
               DISPLAY "Fim"            
               STOP " "
               GO 89999-FIM-PROCESSA
            END-IF
+
            IF W-ALUNO = SPACES
-              DISPLAY "Campo obrigatorio"
-                                       
+              DISPLAY "Campo obrigatorio"                            
               STOP " "
               DISPLAY W-BRANCOS        
               GO 20000-PROCESSA
            END-IF
-           .
+      *----------------------------------------------------------------*     .
        20100-ENTRA-NOTA-1.
+      *----------------------------------------------------------------* 
            DISPLAY "Informe a primeira nota do aluno"
                                        
            ACCEPT W-NOTA1              
-           DISPLAY W-BRANCOS           
+           DISPLAY W-BRANCOS 
+
            IF KEY-ESC
               GO 20000-PROCESSA
            END-IF
            
            ADD W-NOTA1                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       
-           .
+           DISPLAY W-TOTAL-NOTAS  
+               
+      *----------------------------------------------------------------*
        20200-ENTRA-NOTA-2.
+      *----------------------------------------------------------------* 
            DISPLAY "Informe a segunda nota do aluno"
                                        
            ACCEPT W-NOTA2              
@@ -89,8 +98,10 @@
            
            ADD W-NOTA2                 TO W-TOTAL-NOTAS
            DISPLAY W-TOTAL-NOTAS       
-           .
+           
+      *----------------------------------------------------------------*     
        20300-ENTRA-NOTA-3.
+      *----------------------------------------------------------------* 
            DISPLAY "Informe a terceira nota do aluno"
                                        
            ACCEPT W-NOTA3              
@@ -98,17 +109,21 @@
            
            ADD W-NOTA3                 TO W-TOTAL-NOTAS
            DISPLAY W-TOTAL-NOTAS       
-           .
+           
+      *----------------------------------------------------------------*     
        20400-ENTRA-NOTA-4.
+      *----------------------------------------------------------------* 
            DISPLAY "Informe a primeira nota do aluno"
                                        
            ACCEPT W-NOTA4              
            DISPLAY W-BRANCOS           
            
            ADD W-NOTA4                 TO W-TOTAL-NOTAS
-           DISPLAY W-TOTAL-NOTAS       
-           .
+           DISPLAY W-TOTAL-NOTAS   
+
+      *----------------------------------------------------------------*
        20500-CALCULA-MEDIA.
+      *----------------------------------------------------------------* 
            DIVIDE W-TOTAL-NOTAS        BY 4
                                    GIVING W-MEDIA
            DISPLAY W-MEDIA             
@@ -123,8 +138,10 @@
                  DISPLAY "APROVADO!!!"
               END-IF
            END-IF
+
            STOP " "
-           .
+
+      *----------------------------------------------------------------*
        89999-FIM-PROCESSA.
            EXIT.
       *----------------------------------------------------------------*
